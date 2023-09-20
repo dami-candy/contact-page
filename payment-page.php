ab381ca413a691827efe844b1a8e9f4e86c0394d
Dami Sadiq <dami@candymarketing.co.uk>
2023-09-20 21:44:21 +0100

    creating payment page

---
+++ payment-page.php
@@ -0,0 +1,90 @@
+<?php /* Template Name: Payment Page Template*/ ?>
+
+<?php get_header();?>
+    <section id="payment-page">
+        <div class="container"><h1><?php the_title(); ?></h1>
+            <div class="row">
+                <div class="d-flex flex-wrap justify-content-between">
+                    <div class="col-12 col-md-5">
+                        <h2>Card Payment</h2>
+                        <p>Pay with debit/credit card over the phone Call. 
+                            Card payments will show on your bank statement as "OMS".</p>
+                            <a><div class="btn">Call us  0800 069 9269</div></a>
+                    </div>
+                    <div class="col-12 col-md-5">
+                        <h2>Card Payment</h2>
+                        <p>Pay with debit/credit card over the phone Call. 
+                            Card payments will show on your bank statement as "OMS".</p>
+                            <div></div>
+                    </div>
+                    <div class="col-12 col-md-5 mt-5">
+                        <h2>Card Payment</h2>
+                        <p>Pay with debit/credit card over the phone Call. 
+                            Card payments will show on your bank statement as "OMS".</p>
+                            <div></div>
+                    </div>
+                    <div class="col-12 col-md-5 mt-5">
+                        <h2>Card Payment</h2>
+                        <p>Pay with debit/credit card over the phone Call. 
+                            Card payments will show on your bank statement as "OMS".</p>
+                            <a><div class="btn">Call us  0800 069 9269</div></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row half-section mt-5">
+            <div class="container d-flex flex-wrap">
+                <div class="col-12 col-md-6">
+                <img class="header-logo" src="<?php bloginfo('template_url'); ?>/images/div.png" alt="Spore Buddies Logo">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h2 class="mb-5">Order Discounts</h2>
+                    <div class="d-flex flex-wrap mb-4">
+                        <div class="col-1 d-flex"><span class="icon"></span></div>
+                        <div class="col-11">
+                            <h3>Pay with Cash - 10% off</h3>
+                            <div>Sending cash allows for a 10% discount. Use code: C10 
+                            We can not accept any of the old-style bank notes after 20th Sept 2022.</div>
+                        </div>
+                        
+                    </div>
+                    <div class="d-flex flex-wrap mb-4">
+                        <div class="col-1 d-flex"><span class="icon"></span></div>
+                        <div class="col-11">
+                            <h3>Pay with Cash - 10% off</h3>
+                            <div>Sending cash allows for a 10% discount. Use code: C10 
+                            We can not accept any of the old-style bank notes after 20th Sept 2022.</div>
+                        </div>
+                        
+                    </div>
+                    <div class="d-flex flex-wrap mb-4">
+                        <div class="col-1 d-flex"><span class="icon"></span></div>
+                        <div class="col-11">
+                            <h3>Pay with Cash - 10% off</h3>
+                            <div>Sending cash allows for a 10% discount. Use code: C10 
+                            We can not accept any of the old-style bank notes after 20th Sept 2022.</div>
+                        </div>
+                        
+                    </div>
+                    <div class="d-flex flex-wrap mb-4">
+                        <div class="col-1 d-flex"><span class="icon"></span></div>
+                        <div class="col-11">
+                            <h3>Pay with Cash - 10% off</h3>
+                            <div>Sending cash allows for a 10% discount. Use code: C10 
+                            We can not accept any of the old-style bank notes after 20th Sept 2022.</div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php 
+        the_content();
+        ?>
+        </div>
+    </section>
+
+</div>
+<?php get_footer();?>
+
+
